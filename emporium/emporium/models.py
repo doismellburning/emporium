882d6599e51bc04297d4273b726c5e49d0c567be
Kristian Glass <git@doismellburning.co.uk>
2018-12-07 14:15:36 +0000

    Package.fetch_latest_version

--- emporium/emporium/models.py
+++ emporium/emporium/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 
+import requests
+
 
 class Package(models.Model):
     name = models.CharField(max_length=120)
 
     def __str__(self):
         return self.name
+
+    def fetch_latest_version(self) -> ("PackageVersion", bool):
+        """
+        Checks PyPI for the latest version, then get_or_create-s an appropriate PackageVersion
+        """
+        resp = requests.get("https://pypi.org/pypi/%s/json" % self.name)
+        resp.raise_for_status()
+
+        try:
+            releases = resp.json()["releases"]
+            latest_version = list(releases)[-1]
+            pv = PackageVersion.objects.get_or_create(
+                package=self, version=latest_version
+            )
+            return pv
+        except (KeyError, IndexError):
+            return None
 
 
 class PackageVersion(models.Model):
