02b24f9e72ae2d9e34893cb2628151d5444036bc
Kristian Glass <git@doismellburning.co.uk>
2018-12-07 13:36:28 +0000

    __str__ for models
    I'm going to regret not testing this / enforcing 100% coverage aren't I?

--- emporium/emporium/models.py
+++ emporium/emporium/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Package(models.Model):
     name = models.CharField(max_length=120)
 
+    def __str__(self):
+        return self.name
+
 
 class PackageVersion(models.Model):
     package = models.ForeignKey("package", on_delete=models.CASCADE)
     version = models.CharField(max_length=120)
     setuppy = models.TextField()
     fetched = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.package) + "-" + self.version
