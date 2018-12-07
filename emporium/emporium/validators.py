e3ba2702c2865017ebb999d4c4c37b5cc2629842
Kristian Glass <git@doismellburning.co.uk>
2018-12-07 17:13:25 +0000

    Validate that Package.name exists on PyPI

---
+++ emporium/emporium/validators.py
@@ -0,0 +1,9 @@
+from django.core.exceptions import ValidationError
+
+import requests
+
+
+def validate_package_name_exists(name):
+    resp = requests.get("https://pypi.org/pypi/%s/json" % name)
+    if resp.status_code != 200:
+        raise ValidationError("%(name)s not found on PyPI", params={"name": name})
