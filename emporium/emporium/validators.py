b6677010b2d4124e960b4d13cdc2863f37237f29
Kristian Glass <git@doismellburning.co.uk>
2018-12-12 19:53:02 +0000

    Don't follow redirects when validating package names with PyPI
    Because PyPI will correct `django` to `Django`

--- emporium/emporium/validators.py
+++ emporium/emporium/validators.py
@@ -3,6 +3,9 @@ from django.core.exceptions import ValidationError
 
 
 def validate_package_name_exists(name):
-    resp = requests.get("https://pypi.org/pypi/%s/json" % name)
+    resp = requests.get(
+        "https://pypi.org/pypi/%s/json" % name,
+        allow_redirects=False,  # PyPI will correct case with redirects, and argh
+    )
     if resp.status_code != 200:
         raise ValidationError("%(name)s not found on PyPI", params={"name": name})
